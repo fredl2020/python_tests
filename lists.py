06be8b71a3a0db2e6c7ec43430a41d712709715b
poseidon <fredlimat@gmail.com>
2020-12-14 15:57:00 +0100

    news files created for testings

--- lists.py
+++ lists.py
@@ -1,2 +1,17 @@
 names = ["Fred","BB","Laurence"]
+
 print(names[1])
+
+names.append("Léa")
+
+print(names[3])
+
+names.sort()
+
+print(names)
+
+simple_name = input("Add a name ")
+
+names.append(simple_name)
+
+print(f"The list has {len(names)} elements : {names}")
